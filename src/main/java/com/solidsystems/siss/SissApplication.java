7b2272bf2f4d3e4d4dc20c9c9643a4aa0a348bce
Andrey <asd@asd.com>
2021-08-05 21:48:11 +0300

    Initial commit

---
+++ src/main/java/com/solidsystems/siss/SissApplication.java
@@ -0,0 +1,13 @@
+package com.solidsystems.siss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SissApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SissApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solidsystems/siss/SissApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solidsystems.siss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SissApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
