7379b1e18a33e5c6aba3e26e2cad2eb98aaf0d55
Andrey <asd@asd.com>
2021-08-13 01:24:42 +0300

    Add @Service

--- src/main/java/com/solidsystems/siss/dao/AddProductDB.java
+++ src/main/java/com/solidsystems/siss/dao/AddProductDB.java
@@ -3,7 +3,9 @@ package com.solidsystems.siss.dao;
 import com.solidsystems.siss.controller.model.Product;
 import com.solidsystems.siss.dao.model.ProductDB;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AddProductDB {
     @Autowired
     public ProductDB productDB;
