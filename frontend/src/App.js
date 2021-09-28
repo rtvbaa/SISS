ba8eab3c8f6697a4edd201ad0a986a187e525e38
Andrey <local@local.local>
2021-09-28 18:44:43 +0300

    Added react-router-dom and new pages.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import {useEffect, useState} from 'react';
 import ProductTable from './ProductTable'
-
-const defaultItems = [{id: '1', productName: 'name', productPrice: 23}];
+import Header from './Header.js';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
     const handleClick = () => {
@@ -14,10 +14,10 @@ function App() {
     // const array = useState(defaultItems);
     // const items = array[0];
     // const setItems = array[1];
-    const [items, setItems] =  useState(defaultItems);
+    const [items, setItems] = useState([]);
 
-            // hook
-    useEffect(()=>{
+    // hook
+    useEffect(() => {
         getData().then(data => {
             setItems(data);
         })
@@ -25,22 +25,48 @@ function App() {
 
     return (
         <div className="App">
-            <header className="App-header">
-                <div className="container">
-                    <ProductTable items={items}/>
-                </div>
-                <div>
-                    <button id="the-button" onClick={handleClick}>
-                        Add a new product
-                    </button>
-                </div>
-            </header>
+            <Router>
+                <header className="App-header"><Header/></header>
+                <Switch>
+                    <Route exact path="/">
+                        <body className="App-body">
+                        <div className="container">
+                            <ProductTable items={items}/>
+                        </div>
+
+                        <div>
+                            <button id="the-button" onClick={handleClick}>
+                                Add a new product
+                            </button>
+                        </div>
+                        </body>
+                    </Route>
+                    <Route path="/Sale">
+                        <body className="App-body">
+                            <p>Sale</p>
+                        </body>
+                    </Route>
+                    <Route path="/Discount">
+                        <body className="App-body">
+                        <p>Discount</p>
+                        </body>
+                    </Route>
+                </Switch>
+
+            </Router>
+
+
+
+
+
         </div>
     );
 }
 
 function getData() {
- return fetch("http://localhost:8080/get").then((res) => {return res.json();});
+    return fetch("http://localhost:8080/get").then((res) => {
+        return res.json();
+    });
 }
 
 
@@ -65,6 +91,6 @@ function sendPost() {
         .then(() => {
             window.location.reload();
         })
-}   
+}
 
 export default App;
---
+++ frontend/src/Header.js
@@ -0,0 +1,12 @@
+
+function Header() {
+    return (<div>
+        <a href='/'>Product</a>
+        <a> </a>
+        <a href='/Sale'>Sale</a>
+        <a> </a>
+        <a href='/Discount'>Discount</a>
+    </div>);
+}
+
+export default Header;
