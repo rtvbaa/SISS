5a7794ea3cc7e7e62772f56e0b0716f95abc55e5
Andrey <local@local.local>
2021-10-01 20:29:34 +0300

    Added DiscountPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from './Header.js';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import ProductPage from "./ProductPage";
+import DiscountPage from "./DiscountPage"
 
 function App() {
     return (
@@ -19,7 +20,7 @@ function App() {
                     </Route>
                     <Route path="/Discount">
                         <body className="App-body">
-                        <p>Discount</p>
+                        <DiscountPage/>
                         </body>
                     </Route>
                 </Switch>
---
+++ frontend/src/DiscountPage.js
@@ -0,0 +1,41 @@
+import {useEffect, useState} from 'react';
+
+function DiscountPage() {
+
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        getData().then(data => {
+            setItems(data);
+        })
+    }, [setItems]);
+
+    return(
+        <body className="App-body">
+        <div className="container">
+            <table className="table">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Percentage</th>
+                    <th>Discount hour</th>
+                    <th>Product Id</th>
+                </tr>
+                </thead>
+                <tbody id="data">
+                {items.map(itemData => (<tr><td>{itemData.id}</td><td>{itemData.discountPercentage}</td><td>{new Date(itemData.discountDate).getHours()}</td><td>{itemData.productId}</td></tr>))}
+                </tbody>
+            </table>
+        </div>
+        </body>
+    )
+
+    function getData() {
+        return fetch("http://localhost:8080//getDiscount").then((res) => {
+            return res.json();
+        });
+    }
+
+}
+
+export default DiscountPage;
