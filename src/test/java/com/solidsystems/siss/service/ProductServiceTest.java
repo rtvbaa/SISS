7f329db2ac0390c5c3f3b5c1cdc1af124e56fbf4
Andrey <local@local.local>
2021-12-02 16:19:00 +0300

    Added ProductServiceTest getAll

---
+++ src/test/java/com/solidsystems/siss/service/ProductServiceTest.java
@@ -0,0 +1,59 @@
+package com.solidsystems.siss.service;
+
+import com.solidsystems.siss.dao.ProductRepository;
+import com.solidsystems.siss.dao.model.ProductEntity;
+import com.solidsystems.siss.dao.model.SaleEntity;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductServiceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @MockBean
+    ProductRepository productRepository;
+
+    ProductEntity productEntity1 = new ProductEntity("Meat", 1000, Arrays.asList(new SaleEntity(), new SaleEntity()));
+    ProductEntity productEntity2 = new ProductEntity("Fish", 900, Arrays.asList(new SaleEntity(), new SaleEntity()));
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void getAll() throws Exception {
+        List<ProductEntity> records = new ArrayList<>(Arrays.asList(productEntity1, productEntity2));
+        Mockito.when(productRepository.findAll()).thenReturn(records);
+        this.mockMvc.perform(MockMvcRequestBuilders
+                        .get("/getProducts")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$", Matchers.hasSize(2)))
+                .andExpect(jsonPath("$[0].productName", Matchers.is("Meat")))
+                .andExpect(jsonPath("$[1].productName", Matchers.is("Fish")));
+    }
+}
