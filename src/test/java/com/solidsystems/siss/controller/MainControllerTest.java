2fd90e55c42d6272d642cfe5ca2ea56e6f74152a
Andrey <local@local.local>
2021-12-03 16:49:42 +0300

    @SpringBootTest replaced by @WebMvcTest

--- src/test/java/com/solidsystems/siss/controller/MainControllerTest.java
+++ src/test/java/com/solidsystems/siss/controller/MainControllerTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@WebMvcTest
 @AutoConfigureMockMvc
 class MainControllerTest {
 
