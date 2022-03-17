f0dfabbbb405ee31aa9dc6ec7d0db8ce63230859
Velbot <43761239+rtvbaa@users.noreply.github.com>
2022-03-17 17:51:14 +0300

    made the ProductServiceTest independent of the database

--- src/test/java/com/solidsystems/siss/service/ProductServiceTest.java
+++ src/test/java/com/solidsystems/siss/service/ProductServiceTest.java
@@ -4,28 +4,28 @@ import com.solidsystems.siss.dao.ProductRepository;
 import com.solidsystems.siss.dao.model.ProductEntity;
 import com.solidsystems.siss.dao.model.SaleEntity;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
 
-    @Autowired
+    @Mock
+    private ProductRepository productRepository;
     private ProductService productService;
 
-
-    @MockBean
-    ProductRepository productRepository;
+    @BeforeEach
+    void setUp() {
+        productService = new ProductService(productRepository);
+    }
 
     ProductEntity productEntity1 = new ProductEntity("Meat", 1000, Arrays.asList(new SaleEntity(), new SaleEntity()));
     ProductEntity productEntity2 = new ProductEntity("Fish", 900, Arrays.asList(new SaleEntity(), new SaleEntity()));
